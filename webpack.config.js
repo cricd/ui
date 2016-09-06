7a9ad780bbd7902893322087836fb47a78cea4c1
Bradley Scott <bradley.s.scott@gmail.com>
2016-09-06 20:18:53 +1200

    Introduced config variables

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,18 @@ var webpack = require('webpack');
 module.exports = {
     entry: './src/index.js',
     output: { path: './build/', filename: 'index.js' },
-    
+    plugins: [
+        new webpack.DefinePlugin({
+            '__API_URL__': JSON.stringify('localhost'),
+        })
+    ],
     module: {
         loaders: [
-             { test: /\.js$/,
-                 exclude: /node_modules/, 
-                 loader: 'babel-loader?presets[]=es2015&presets[]=react'
-             },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader?presets[]=es2015&presets[]=react'
+            },
             {
                 test: /(\.scss|\.css)$/,
                 loaders: ['style', 'css']
