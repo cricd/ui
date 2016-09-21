8155b8f7a961e3f08688ff952dcc131b124407c1
Ryan Scott <ryankennethscott@gmail.com>
2016-09-21 13:09:58 +1200

    Adding initial Scoring functionality

---
+++ src/ScoreMatch/ScoreMatch.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import './ScoreMatch.scss';
+
+class ScoreMatch extends Component {
+    render() {
+        return (
+            <div>This is a match</div>
+        );
+    }
+}
+
+export default ScoreMatch;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './Shared/App/App';
 import View from './ViewScore/View';
+import ScoreMatch from './ScoreMatch/ScoreMatch';
 import './index.scss';
 
 
@@ -10,6 +11,7 @@ render((
     <Router history={hashHistory}>
         <Route path="/" component={App}>
                 <Route path="/view/:matchID" component={View} />
+                <Route path="/score/:matchID" component={ScoreMatch} />
         </Route>
     </Router>
 ), document.getElementById('app'))
