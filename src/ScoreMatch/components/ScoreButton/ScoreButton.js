e4e7f35b6ba212ccaaf8d23e9fa5172dc79cd1a8
Ryan Scott <ryankennethscott@gmail.com>
2016-09-25 12:10:10 +1300

    Updating global font to Source Sans Pro and fixing score components

--- src/ScoreMatch/components/ScoreButton/ScoreButton.js
+++ src/ScoreMatch/components/ScoreButton/ScoreButton.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import './ScoreButton.scss';
+import { Button } from 'rebass';
 
 class ScoreButton extends Component {
     render() {
         return (
-            <div></div>
-        );
+            <div>
+                <Button
+                    backgroundColor="secondary"
+                    color="white"
+                    inverted
+                    rounded
+                    >
+                    {this.props.buttonText}
+                </Button>
+            </div>
+        )
     }
-}
-
+};
 export default ScoreButton;
