754e4deb97780f16580befe772656cd6b6d9e1cb
Ryan Scott <ryankennethscott@gmail.com>
2016-09-26 20:53:03 +1300

    Implementing create match functionality

--- src/CreateMatch/CreateMatch.js
+++ src/CreateMatch/CreateMatch.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
 import './CreateMatch.scss';
-import TeamSuggest from './components/TeamSuggest';
 import DatePicker from 'material-ui/DatePicker';
 import Paper from 'material-ui/Paper';
+import AutoComplete from 'material-ui/AutoComplete';
+import FlatButton from 'material-ui/FlatButton';
+
+const teams = [
+]
 
 class CreateMatch extends Component {
+    constructor() {
+        super()
+        fetch("http://localhost:1337/teams").then(
+            function (response) {
+                return response.json();
+            }).then(function (res) {
+                for (var i = 0; i < res.length; i++) {
+                    teams.push(res[i]["name"])
+                }
+            });
+            this.state = {
+                homeTeam: "",
+                awayTeam: "",
+                matchDate: ""
+            }
 
+            this.handleHomeTeam = this.handleHomeTeam.bind(this)
+            this.handleAwayTeam = this.handleAwayTeam.bind(this)
+
+
+    }
+    handleHomeTeam(input) {
+        this.setState({homeTeam: input})
+   }
+    handleAwayTeam(input) {
+        this.setState({awayTeam: input})
+   }
+
+    createMatch(index) {
+        console.log(index);
+    }
 
     render() {
         return (
@@ -14,11 +48,29 @@ class CreateMatch extends Component {
                     Create Match
                 </h2>
                 <h3> Home team </h3>
-                <TeamSuggest />
+                <AutoComplete
+                    name="homeTeam"
+                    hintText="Select the home team"
+                    dataSource={teams}
+                    filter={AutoComplete.fuzzyFilter}
+                    onNewRequest={this.handleHomeTeam}
+                    />
                 <h3> Away team </h3>
-                <TeamSuggest />
+                <AutoComplete
+                    name="awayTeam"
+                    hintText="Select the away team"
+                    dataSource={teams}
+                    filter={AutoComplete.fuzzyFilter}
+                    onNewRequest={this.handleAwayTeam}
+                    />
                 <h3> Match date </h3>
-                <DatePicker  />
+                <DatePicker
+                    autoOk={true}
+                    />
+                     <FlatButton 
+                     label="Create" 
+                     onClick={this.createMatch}
+                     />
             </div>
         );
     }
