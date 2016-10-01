2d2a35dca37806da19a34d5c5a80213cdb3f1acf
Bradley Scott <bradley.s.scott@gmail.com>
2016-10-01 17:33:18 +1300

    Added npm run dev script

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,9 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       '__SCOREPROCESSOR_URL__': JSON.stringify(process.env.SCOREPROCESSOR_URL) || JSON.stringify('score-processor:3002'),
+      '__BATSMANINNINGSPROCESSOR_URL__': JSON.stringify(process.env.BATSMANCHANGEPROCESSOR_URL) || JSON.stringify('batsman-innings-processor:3000'),
       '__CHANGEPUBLISHER_URL__': JSON.stringify(process.env.CHANGEPUBLISHER_URL) || JSON.stringify('change-publisher:3100'),
-      '__ENTITYSTORE_URL__': JSON.stringify(process.env.ENTITY_STORE_URL) || JSON.stringify('localhost:1337'),
+      '__ENTITYSTORE_URL__': JSON.stringify(process.env.ENTITYSTORE_URL) || JSON.stringify('entities:1337'),
     }),
   ],
   module: {
