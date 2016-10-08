07e5443e64d681aab575296100dd3c8892766783
Ryan Scott <ryankennethscott@gmail.com>
2016-10-08 13:44:49 +1300

    Adding selecting a batting team

--- src/ScoreMatch/ScoreMatch.js
+++ src/ScoreMatch/ScoreMatch.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './ScoreMatch.scss';
 import Divider from 'material-ui/Divider';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 import MatchInfo from '../Shared/MatchInfo/MatchInfo';
 import InningsStats from '../Shared/InningsStats/InningsStats';
 import io from 'socket.io-client';
@@ -9,7 +13,48 @@ class ScoreMatch extends Component {
     constructor(props) {
         super(props)
         //Get the match ID
-        this.state = {}
+        this.state = {
+            teams: [],
+            battingTeam: {},
+            bowlingTeam: {},
+            currentInnings: 1,
+            striker: {},
+            nonStriker: {},
+            bowler: {},
+            dialogOpen: false,
+            selectedTeam: 0,
+    }
+    this.handleBattingTeamChange = this.handleBattingTeamChange.bind(this);
+    this.handleDialogClose = this.handleDialogClose.bind(this);
+    }
+
+    handleDialogOpen() {
+        this.setState({ dialogOpen: true });
+    };
+
+    handleDialogClose() {
+        this.setState({ dialogOpen: false });
+    };
+
+
+    getTeams() {
+        console.log(this.props)
+        var matchId = this.props.params.matchId
+        var entitiesUrl = 'http://' + __ENTITYSTORE_URL__
+        fetch(entitiesUrl + '/matches/' + matchId)
+            .then(response => { return response.json(); })
+            .then(json => {
+                this.setState({
+                    teams: [json.awayTeam, json.homeTeam],
+                    dialogOpen: true
+                })
+                console.log(json)
+            })
+            .catch(error => { console.log(error); });
+    }
+
+    getBatsmen() {
+        //  team/id/players/
     }
 
     getScore() {
@@ -36,22 +81,47 @@ class ScoreMatch extends Component {
     }
 
     componentDidMount() {
-        this.getScore();
-        this.subscribeToMatchEvents();
+        this.getTeams();
+        //        this.getScore();
+        //      this.subscribeToMatchEvents();
     }
 
+        handleBattingTeamChange(event, index, value) {
+            this.setState({selectedTeam: value});
+        }
 
     render() {
-        var innings;
-        if (this.state.innings) {
-            var lastInnings  = this.state.innings.length
-            innings =  <InningsStats  sm={12} md={6} {...this.state.innings[lastInnings - 1]} key={lastInnings} innings={lastInnings} />;
-        }
+        const actions = [
+            <FlatButton
+                label="Ok"
+                primary={true}
+                keyboardFocused={true}
+                onTouchTap={this.handleDialogClose}
+                />,
+        ];
+        var menuItems = this.state.teams ? this.state.teams.map((team, key) => {return  <MenuItem value={team} primaryText={team.name} key={key}/>}) : [];
+
+        // [Batsman] scored [0-9] [runs/leg byes/wides] off the bowling of [Bowler]
+        // [Batsman] was dismissed by [run out/bowled/lbw...]  [by fielder] off the bowling of [Bowler]
+        // var innings;
+        // if (this.state.innings) {
+        //     var lastInnings  = this.state.innings.length
+        //     innings =  <InningsStats  sm={12} md={6} {...this.state.innings[lastInnings - 1]} key={lastInnings} innings={lastInnings} />;
+        // }
         return (
             <div>
+                <Dialog
+                    title="Select batting team"
+                    open={this.state.dialogOpen}
+                    actions={actions}
+                    modal={false}
+                    >
+                    <SelectField value={this.state.selectedTeam} onChange={this.handleBattingTeamChange}>
+                        {menuItems}
+                    </SelectField>
+                </Dialog>
                 <MatchInfo  {...this.state.matchInfo} />
                 <Divider/>
-                {innings}
                 <h3> Runs </h3>
                 <div className="cricd-scoreMatch-runsContainer">
                 </div>
