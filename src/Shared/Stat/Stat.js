d6b91fff5e4d5e27a23bc8debd52712c008527e8
Bradley Scott <bradley.s.scott@gmail.com>
2017-01-30 21:24:21 +1300

    Used function syntax

--- src/Shared/Stat/Stat.js
+++ src/Shared/Stat/Stat.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import './Stat.scss';
 
-class Stat extends Component {
-
-    render() {
-        return (
-            <span className="cricd-stat">
-                <span className="cricd-stat-value">{this.props.children}</span>
-                <span className="cricd-stat-units">{this.props.units}</span>
-            </span>
-        );
-    }
+function Stat(props) {
+    return (
+        <span className="cricd-stat">
+            <span className="cricd-stat-value">{props.children}</span>
+            <span className="cricd-stat-units">{props.units}</span>
+        </span>
+    );
 }
 
 export default Stat;
