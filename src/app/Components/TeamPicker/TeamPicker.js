5880771c9575d80730dbee94555f2c453459eb50
Bradley Scott <bradley.s.scott@gmail.com>
2017-02-22 08:43:20 +1300

    Half finished re-write of Team Picker

--- src/app/Components/TeamPicker/TeamPicker.js
+++ src/app/Components/TeamPicker/TeamPicker.js
@@ -2,18 +2,31 @@ import React, { Component } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import FlatButton from 'material-ui/FlatButton';
 import AutoComplete from 'material-ui/AutoComplete';
+import TextField from 'material-ui/TextField';
+import { List, ListItem, makeSelectable } from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
 import moment from 'moment';
 import { inject, observer } from 'mobx-react';
-import { action } from 'mobx';
+import { action, observable, computed } from 'mobx';
 
 @inject('teamStore')
 @observer class TeamPicker extends Component {
-    
+    @observable filterText = '';
+    @computed get filteredTeams() {
+        let teams = this.props.teamStore.teams.toJS();
+        let filtered = teams.filter((team) => { return team.name.toLowerCase().includes(this.filterText.toLowerCase()) });
+        let numberOfItems = Math.min(5, filtered.length);
+        filtered = filtered.sort((team) => { return team.name });
+        return filtered.slice(0, numberOfItems);
+    };
+
     constructor(props) {
         super(props);
         this.onTeamChanged = this.onTeamChanged.bind(this);
+        this.onFilterChanged = this.onFilterChanged.bind(this);
     }
 
+    @action onFilterChanged = function(object, filter) { this.filterText = filter; }
     @action onTeamChanged = function(chosenTeam, index) {
         if(index === -1) { // New team
             let newTeam = { name: chosenTeam };
@@ -26,17 +39,32 @@ import { action } from 'mobx';
     }
 
     render() {
-        let teams = this.props.teamStore.teams.toJS(); // Autocomplete can recognise array type
+        let teamItems = this.filteredTeams.map((t, i) => {
+            return <ListItem
+                value={t.id}
+                key={i}
+                primaryText={t.name}
+                leftAvatar={<Avatar>{t.name.substring(0, 2)}</Avatar>}
+            />
+        });
+        if(this.filterText.length > 3) teamItems.push(<ListItem
+                key={-1}
+                value={-1}
+                primaryText={'Add ' + this.filterText + '...'}
+                leftAvatar={<Avatar>+</Avatar>}
+            />)
+        let SelectableList = makeSelectable(List);
+
         return (
             <div>
-                <AutoComplete
+                <TextField
                     hintText={this.props.hint}
-                    dataSource={teams}
-                    dataSourceConfig={{ text: 'name', value: 'id' }}
-                    onNewRequest={this.onTeamChanged}
-                    filter={AutoComplete.fuzzyFilter}
-                    maxSearchResults={5}
+                    defaultValue={this.filterText}
+                    onChange={this.onFilterChanged}
                 />
+                <SelectableList>
+                    {teamItems}
+                </SelectableList>
             </div>
         );
     }
@@ -48,7 +76,7 @@ TeamPicker.propTypes = {
 }
 
 TeamPicker.defaultProps = {
-    hintText: 'Search for team',
+    hintText: 'Search for team...',
     onTeamPicked: () => { console.log('Team changed') }
 }
 
