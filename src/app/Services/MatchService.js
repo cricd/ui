73e8aef4b406d7c16d3549488def886a2763982f
Bradley Scott <bradley.s.scott@gmail.com>
2017-02-22 08:42:51 +1300

    Better failure handling

--- src/app/Services/MatchService.js
+++ src/app/Services/MatchService.js
@@ -4,10 +4,11 @@ import request from 'superagent';
 const entityStoreUrl = 'http://' + __ENTITYSTORE_URL__;
 const changePublisherUrl = 'http://' + __CHANGEPUBLISHER_URL__;
 const scoreProcessorUrl = 'http://' + __SCOREPROCESSOR_URL__;
+const nextBallProcessorUrl = 'http://' + __NEXTBALLPROCESSOR_URL__;
 
 export default class MatchService {
 
-    constructor() {}
+    constructor() { }
 
     getMatchInfo(matchId, callback) {
         if(!matchId) {
@@ -19,8 +20,11 @@ export default class MatchService {
         request
             .get(entityStoreUrl + '/matches/' + matchId)
             .end((err, res) => {
-                if(err) return callback(err);
-                else if(!res.ok) return callback(res.statusText);
+                if(err && err.status === 404) return callback('No match with this id exists');
+                else if(err) {
+                    console.error(err);
+                    return callback('An error occurred trying to get Match info');
+                }
                 callback(null, res.body);
             });
     };
@@ -34,6 +38,7 @@ export default class MatchService {
 
         let socket = io.connect(changePublisherUrl + '?match=' + matchId);
         socket.on('score-change', handler);
+        console.debug('Subscription to match ' + matchId + ' successful');
         callback();
     };
 
@@ -48,8 +53,34 @@ export default class MatchService {
             .get(scoreProcessorUrl)
             .query({ match: matchId })
             .end((err, res) => {
-                if(err) return callback(err);
-                else if(!res.ok) return callback(res.statusText);
+                if(err && err.status === 404) return callback('No score associated with this matchId');
+                else if(err) {
+                    console.error(err);
+                    return callback('An error occurred trying to retrieve the score');
+                }
+                callback(null, res.body);
+            });
+    };
+
+    getNextMatchEvent(matchId, callback) {
+        if(!matchId) {
+            let message = 'matchId is required to retrieve next ball';
+            console.error(message);
+            return callback(message);
+        }
+
+        request
+            .get(nextBallProcessorUrl)
+            .query({ match: matchId })
+            .end((err, res) => {
+                if(err && err.status === 404) {
+                    console.debug('No matchEvents associated with this matchId');
+                    return callback();
+                }
+                if(err) {
+                    console.error(err);
+                    return callback('An error occurred trying to retrieve the score');
+                }
                 callback(null, res.body);
             });
     };
@@ -59,8 +90,10 @@ export default class MatchService {
             .post(entityStoreUrl + '/matches/')
             .send(match)
             .end((err, res) => {
-                if(err && err.response.body.originalError) return callback(err.response.body.originalError);
-                else if(err) return callback(err.message);
+                if(err) {
+                    console.error(err);
+                    return callback('An error occurred trying to create Match');
+                }
                 callback(null, res.body);
             });
     }
