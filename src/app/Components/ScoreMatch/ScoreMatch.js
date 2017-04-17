526136ca6e5df4b7200fd19f080fed61b3bdfa58
Bradley Scott <Bradley Scott>
2017-04-17 17:02:11 +1200

    Progress on scoring

--- src/app/Components/ScoreMatch/ScoreMatch.js
+++ src/app/Components/ScoreMatch/ScoreMatch.js
@@ -11,9 +11,34 @@ import BowlingCard from '../BowlingCard/BowlingCard';
 import BattingCard from '../BattingCard/BattingCard';
 import PlayerPicker from '../PlayerPicker/PlayerPicker';
 import Spinner from '../Spinner/Spinner';
+import './ScoreMatch.scss';
+import _ from 'underscore';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import Subheader from 'material-ui/Subheader';
+import ordinal from 'ordinal-number-suffix';
+import Stat from '../Stat/Stat';
 
 @inject('matchStore', 'uiStateStore')
 @observer class ScoreMatch extends Component {
+
+    displayRules = { // Determines which UI components to display
+        delivery: { runs: true, wickets: false },
+        noBall: { runs: true, wickets: true, wicketsFilter: ['runOut', 'obstruction', 'doubleHit', 'handledBall'] },
+        wide: { runs: true, wickets: true, wicketsFilter: ['runOut', 'stumped', 'handledBall', 'hitWicket', 'obstruction'] },
+        bye: { runs: true, wickets: true, wicketsFilter: ['runOut', 'handledBall', 'obstruction'] },
+        legBye: { runs: true, wickets: true, wicketsFilter: ['runOut', 'handledBall', 'obstruction'] },
+        bowled: { runs: false, wickets: true },
+        timedOut: { runs: false, wickets: true },
+        caught: { runs: false, wickets: true, fielder: true },
+        handledBall: { runs: true, wickets: true },
+        doubleHit: { runs: false, wickets: true },
+        hitWicket: { runs: false, wickets: true },
+        lbw: { runs: false, wickets: true },
+        obstruction: { runs: true, wickets: true },
+        runOut: { runs: false, wickets: true, fielder: true, batsman: true },
+        stumped: { runs: false, wickets: true, fielder: true }
+    };
 
     componentDidMount() {
         this.props.matchStore.getMatch(
@@ -37,9 +62,9 @@ import Spinner from '../Spinner/Spinner';
         let fieldingPlayers = selectedMatch.fieldingTeam ? selectedMatch.fieldingTeam.players.toJS() : [];
         let suggestedBowler = selectedMatch.nextMatchEvent ? selectedMatch.nextMatchEvent.bowler : null;
         let suggestedStrikers = [];
-        if(selectedMatch.batsmen) {
-            if(selectedMatch.batsmen.striker) suggestedStrikers.push(selectedMatch.batsmen.striker.batsman);
-            if(selectedMatch.batsmen.nonStriker) suggestedStrikers.push(selectedMatch.batsmen.nonStriker.batsman);
+        if (selectedMatch.batsmen) {
+            if (selectedMatch.batsmen.striker) suggestedStrikers.push(selectedMatch.batsmen.striker.batsman);
+            if (selectedMatch.batsmen.nonStriker) suggestedStrikers.push(selectedMatch.batsmen.nonStriker.batsman);
         }
 
         return (
@@ -49,19 +74,82 @@ import Spinner from '../Spinner/Spinner';
                 {this.props.uiStateStore.selectedMatch.result && <MatchResult {...this.props.uiStateStore.selectedMatch.result} />}
                 <Flex wrap col={12}>{innings}</Flex>
                 <Divider />
-                <PlayerPicker
-                    label="Bowler"
-                    selectedPlayer={suggestedBowler}
-                    players={fieldingPlayers}
-                    onChange={(player) => { console.log(player) }}
-                />
-                <PlayerPicker
-                    label="Striker"
-                    selectedPlayer={selectedMatch.batsmen ? selectedMatch.batsmen.striker.batsman : null}
-                    players={suggestedStrikers}
-                    onChange={(player) => { console.log(player) }}
-                />
-                {JSON.stringify()}
+                <div className="scoreMatch">
+                    <div className="cricd-inningsStats-teamName">{selectedMatch.battingTeam ? selectedMatch.battingTeam.name : null}</div>
+                    <div className="cricd-inningsStats-label">{selectedMatch.nextMatchEvent ? ordinal(selectedMatch.nextMatchEvent.ball.innings) : null} innings</div>
+                    <Flex wrap>
+                        <Stat units="innings">{selectedMatch.nextMatchEvent ? ordinal(selectedMatch.nextMatchEvent.ball.innings) : null}</Stat>
+                        <Stat units="over">{selectedMatch.nextMatchEvent ? ordinal(selectedMatch.nextMatchEvent.ball.over) : null}</Stat>
+                        <Stat units="ball">{selectedMatch.nextMatchEvent ? ordinal(selectedMatch.nextMatchEvent.ball.ball) : null}</Stat>
+                    </Flex>
+                    <Flex wrap>
+                        <PlayerPicker
+                            label="Bowler"
+                            selectedPlayer={suggestedBowler}
+                            players={_(fieldingPlayers).sortBy('name')}
+                            onChange={(player) => { console.log(player) }}
+                        />
+                        <PlayerPicker
+                            label="Striker"
+                            selectedPlayer={selectedMatch.batsmen ? selectedMatch.batsmen.striker.batsman : null}
+                            players={suggestedStrikers}
+                            onChange={(player) => { console.log(player) }}
+                        />
+                    </Flex>
+                    <Flex wrap>
+                        <SelectField
+                            floatingLabelText="Extras"
+                            onChange={(delivery) => { console.log(delivery) }}
+                        >
+                            <MenuItem value="delivery" primaryText="No extras" />
+                            <Subheader>Extras</Subheader>
+                            <MenuItem value="noBall" primaryText="No ball" />
+                            <MenuItem value="wide" primaryText="Wide" />
+                            <MenuItem value="bye" primaryText="Bye" />
+                            <MenuItem value="legBye" primaryText="Leg Bye" />
+                        </SelectField>
+                        <SelectField
+                            floatingLabelText="Dismissal"
+                            onChange={(wicket) => { console.log(wicket) }}
+                        >
+                            <MenuItem value="delivery" primaryText="No dismissal" />
+                            <MenuItem value="bowled" primaryText="Bowled" />
+                            <MenuItem value="caught" primaryText="Caught" />
+                            <MenuItem value="lbw" primaryText="LBW" />
+                            <MenuItem value="runOut" primaryText="Run out" />
+                            <MenuItem value="stumped" primaryText="Stumped" />
+                            <MenuItem value="hitWicket" primaryText="Hit wicket" />
+                            <MenuItem value="obstruction" primaryText="Obstruction" />
+                            <MenuItem value="timedOut" primaryText="Timed Out" />
+                            <MenuItem value="handledBall" primaryText="Handled ball" />
+                            <MenuItem value="doubleHit" primaryText="Double hit" />
+                        </SelectField>
+                        <SelectField
+                            floatingLabelText="Runs"
+                            onChange={(runs) => { console.log(runs) }}
+                        >
+                            <MenuItem value={0} primaryText="Dot ball" />
+                            <MenuItem value={1} primaryText="One run" />
+                            <MenuItem value={2} primaryText="Two runs" />
+                            <MenuItem value={3} primaryText="Three runs" />
+                            <MenuItem value={4} primaryText="Four runs" />
+                            <MenuItem value={6} primaryText="Six runs" />
+                        </SelectField>
+                        <PlayerPicker
+                            label="Fielder"
+                            selectedPlayer={null}
+                            players={_(fieldingPlayers).sortBy('name')}
+                            onChange={(player) => { console.log(player) }}
+                        />
+                        <PlayerPicker
+                            label="Batsman"
+                            selectedPlayer={selectedMatch.nextMatchEvent ? selectedMatch.nextMatchEvent.batsman : null}
+                            players={suggestedStrikers}
+                            onChange={(player) => { console.log(player) }}
+                        />
+                    </Flex>
+                </div>
+                {JSON.stringify(selectedMatch.nextMatchEvent)}
             </div>
         )
     }
